9f36a281a8152d72a839854b8165e4ef4d9670b7
ryan spangler <ryan.spangler@gmail.com>
2014-03-08 16:29:32 -0800

    removed cljs output

--- resources/public/js/app/out/sol/connect.js
+++
@@ -1,5 +0,0 @@
-goog.provide('sol.connect');
-goog.require('cljs.core');
-goog.require('clojure.browser.repl');
-sol.connect.connect = (function connect(){return clojure.browser.repl.connect.call(null,"http://localhost:9000/repl");
-});
