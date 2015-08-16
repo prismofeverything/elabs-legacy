c69609cc2cc7f97c0a0c100a89e1b2f48eae5136
Ryan Spangler <ryan@getlittlebird.com>
2015-08-15 17:37:18 -0700

    added list and editing

--- src/elephantlaboratories/think.clj
+++ src/elephantlaboratories/think.clj
@@ -15,6 +15,13 @@
   [thought]
   (update-in thought [:words] string/replace #"\n" "<br />"))
 
+(defn read-id
+  [request]
+  (try
+    (Integer/parseInt
+     (-> request :params :thought))
+    (catch Exception e)))
+
 (defn think
   [request]
   (let [thought (first (j/query db ["select * from thought order by random() limit 1"]))]
@@ -23,27 +30,44 @@
 
 (defn thought
   [request]
-  (let [id (try (Integer/parseInt (-> request :params :thought)) (catch Exception e 1))
+  (let [id (or (read-id request) 1)
         thought (first (j/query db ["select * from thought where id = ?" id]))]
     ((page/page "think-thought")
-     (assoc request :thought (insert-newlines thought)))))
+     (assoc request :thought (insert-newlines thought) :id id))))
+
+(defn all
+  [request]
+  (let [thoughts (j/query db ["select * from thought order by id"])]
+    ((page/page "think-all")
+     (assoc request :thoughts (map insert-newlines thoughts)))))
+
+(defn read-thought
+  [request]
+  (if-let [id (read-id request)]
+    (first (j/query db ["select * from thought where id = ?" id]))))
 
 (defn expand
   [request]
-  ((page/page "think-expand")
-   (assoc request :expanding? true)))
+  (let [thought (read-thought request)]
+    ((page/page "think-expand")
+     (assoc request :expanding? true :thought thought))))
 
 (defn mark
   [request]
-  (let [words (-> request :params :words)
-        thought {:words words}
-        mark (first (j/insert! db :thought thought))]
-    {:status 302
-     :headers {"Location" (str "/think/" (:id mark))}
-     :body ""}))
+  (if-let [words (-> request :params :words)]
+    (let [thought {:words words}
+          id (read-id request)
+          mark (if id
+                 (j/update! db :thought thought ["id = ?" id])
+                 (first (j/insert! db :thought thought)))]
+      {:status 302
+       :headers {"Location" (str "/think/" (or id (:id mark)))}
+       :body ""})))
 
 (def think-routes
   ["/think" :think-home #'think
-   [["/expand" :think-expand #'expand]
+   [["/all" :think-all #'all]
     ["/mark" :think-mark {:POST #'mark}]
+    ["/expand" :think-expand #'expand]
+    ["/expand/:thought" :think-enhance #'expand]
     ["/:thought" :think-thought #'thought]]])
